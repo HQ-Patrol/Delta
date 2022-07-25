d0942143c8fcba36fd8a9ba9e2a4a62190fd4711
Leo <leowasntfound@gmail.com>
2022-07-25 21:14:45 +1000

    feat: `EmbedBuilder` addDescription

--- src/components/extenders/EmbedBuilder.js
+++ src/components/extenders/EmbedBuilder.js
@@ -4,3 +4,8 @@ EmbedBuilder.prototype.addField = function addField(name, value, inline) {
   this.addFields({ name, value, inline });
   return this;
 };
+
+EmbedBuilder.prototype.addDescription = function addDescription(append) {
+  this.description = `${this.description ? "" : this.description}\n${append}`;
+  return this;
+};
