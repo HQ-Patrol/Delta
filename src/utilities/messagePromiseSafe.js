5cffebc7bef7467e8f8ebbed7d30ef24e4f0c20c
DoggoKt <mr.doge2019@gmail.com>
2022-07-29 19:57:14 +0200

    fix: make sure our safe wrapper prints the error

--- src/utilities/messagePromiseSafe.js
+++ src/utilities/messagePromiseSafe.js
@@ -2,6 +2,7 @@ module.exports.safe = (cb) => (async (msg) => {
   try {
     cb(msg);
   } catch (e) {
+    console.error(e);
     msg.edit({ content: "Oh no! Something went wrong.", embeds: [] });
   }
 });
