56272ebc10dd9a720abd243abbbe9f1a558b4845
DoggoKt <mr.doge2019@gmail.com>
2022-07-29 19:34:13 +0200

    feat: user-friendly promise templating in EmbedBuilder
    if you need to wait for a promise before sending a message, use setDescription and take use of `{X}` template where X is a number ({0}, {1}...). it will show as loading, and be replaced accordingly upon calling setDescriptionFinished(1,2,...)

--- src/components/extenders/EmbedBuilder.js
+++ src/components/extenders/EmbedBuilder.js
@@ -1,6 +1,23 @@
 const { EmbedBuilder } = require("discord.js");
+const emojis = require("../../constants/emoji");
 
 EmbedBuilder.prototype.addDescription = function addDescription(append) {
   this.data.description = `${this.data.description ? this.data.description : ""}\n${append}`;
+  return this;
+};
+
+const templateRegex = /{\d}/g;
+EmbedBuilder.prototype.setDescription = function setDescriptionPromised(desc) {
+  if (!desc.match(templateRegex)) {
+    this.data.description = desc;
+  } else {
+    this.descriptionPromised = desc;
+    this.data.description = desc.replace(templateRegex, emojis.loading);
+  }
+  return this;
+};
+
+EmbedBuilder.prototype.setDescriptionFinished = function setDescriptionFinished(...reps) {
+  this.data.description = this.descriptionPromised.replace(/{(\d)}/g, (_, i) => reps[i]);
   return this;
 };
