bf967a800cbc7889c5bd380a972aabad8a2d013a
Samuel Echols <samuel.echols@selu.edu>
2022-08-08 00:47:59 -0500

    feat: Added default values to the Economy Model

--- src/database/models/EconomyModel.js
+++ src/database/models/EconomyModel.js
@@ -4,13 +4,12 @@ const {Schema} = mongoose;
 const econ = new mongoose.Schema({
   id: {type: String, ref: "User", index: true},
   lastUse: {type: Schema.Types.Date, default: Date.now()},
-  coins: { type: Number, index: -1 },
-  bank: { type: Number, index: -1 },
-  xp: { type: Number, index: -1 },
-  level: Number,
-  items: Array,
-  default: [],
-  bracket: Number,
+  coins: { type: Number, index: -1, default: 0 },
+  bank: { type: Number, index: -1, default: 0 },
+  xp: { type: Number, index: -1, default: 0 },
+  level: {type: Number, default: 0},
+  items: {type: Array, default: 0},
+  bracket: {type: Number, default: 0},
 });
 
 module.exports = mongoose.model("stats", econ);
