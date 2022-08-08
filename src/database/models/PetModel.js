776ab2dc2cf58d971c468f4c4bb2ab3785a014bb
Samuel Echols <samuel.echols@selu.edu>
2022-08-07 23:44:45 -0500

    feat: added ref to User on id

--- src/database/models/PetModel.js
+++ src/database/models/PetModel.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const PetSchema = new mongoose.Schema({
-  id: String, // Owner
+
+  id: {
+    type: String,
+    ref: "User"
+  }, // Owner
 
   // Pet
   species: { type: String, required: true },
