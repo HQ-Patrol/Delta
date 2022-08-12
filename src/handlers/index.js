6d56cbcd7b77d6971b8aaf42e14bc09cb8bac3db
Leo <leowasntfound@gmail.com>
2022-08-12 12:18:57 +1000

    feat: clean handler
    (1/2) - might implement for ofs but eslint be playing

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,16 +2,18 @@ const allFiles = require("../utilities/allFiles");
 
 const events = (client) => new Promise((resolve) => {
   allFiles("./src/components/events")
-    .filter((file) => file.endsWith(".js"))
+    .filter((file) => file.endsWith(".js") && !file.split("/").at(-1).startsWith("@"))
+    .map((file) => file.replace(/\\/g, "/").replace("src/", ""))
     .forEach((file, i, a) => {
       try {
-        file = file.replace(/\\/g, "/").replace("src/", "");
         const event = require(`../${file}`);
         client.events.set(event.event, event);
         client.on(event.event, (...args) => event.handle(client, ...args));
-        delete require.cache[require.resolve(`../${file}`)];
+
         if (i === a.length - 1) resolve(true);
         client.statistics.events += 1;
+
+        delete require.cache[require.resolve(`../${file}`)];
       } catch (e) {
         console.warn(e);
       }
@@ -20,20 +22,21 @@ const events = (client) => new Promise((resolve) => {
 
 const commands = (client) => new Promise((resolve) => {
   allFiles("./src/components/commands")
-    .filter((file) => file.endsWith(".js"))
+    .filter((file) => file.endsWith(".js") && !file.split("/").at(-1).startsWith("@"))
+    .map((file) => file.replace(/\\/g, "/").replace("src/", ""))
     .forEach((file, i, a) => {
       try {
-        file = file.replace(/\\/g, "/").replace("src/", "");
-        if (file.split("/").at(-1).startsWith("@")) return;
         const command = require(`../${file}`);
         if (!command?.name) return;
         client.commands.set(
           command.name.toLowerCase(),
           command,
         );
-        delete require.cache[require.resolve(`../${file}`)];
+
         if (i === a.length - 1) resolve(true);
         client.statistics.commands += 1;
+
+        delete require.cache[require.resolve(`../${file}`)];
       } catch (e) {
         console.warn(e);
       }
@@ -43,21 +46,22 @@ const commands = (client) => new Promise((resolve) => {
 const extenders = () => new Promise((resolve) => {
   allFiles("./src/components/extenders")
     .filter((file) => file.endsWith(".js"))
+    .map((file) => file.replace(/\\/g, "/").replace("src/", ""))
     .forEach((file, i, a) => {
       try {
-        file = file.replace(/\\/g, "/").replace("src/", "");
-        if (file.split("/").at(-1).startsWith("@")) return;
         require(`../${file}`);
-        delete require.cache[require.resolve(`../${file}`)];
+
         if (i === a.length - 1) resolve(true);
+
+        delete require.cache[require.resolve(`../${file}`)];
       } catch (e) {
         console.warn(e);
       }
     });
 });
 
-module.exports = async (client) => {
-  events(client);
-  commands(client);
-  extenders();
-};
+module.exports = (client) => Promise.all([
+  events(client),
+  commands(client),
+  extenders(),
+]);
