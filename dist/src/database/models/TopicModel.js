b407f15af5ec2c0055a7a082ca4863a739fffff3
Leo <leowasntfound@gmail.com>
2022-08-20 15:34:40 +1000

    delete dist

--- dist/src/database/models/TopicModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const mongoose = require("mongoose");
-const TopicSchema = new mongoose.Schema({
-    UserID: String,
-    topicNo: Number,
-    topic: String,
-    count: Number,
-});
-module.exports = mongoose.model("topic", TopicSchema);
