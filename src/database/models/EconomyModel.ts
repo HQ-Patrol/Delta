58cc73e051c57b3fa98beae4ed7f2c350f8bf2e9
Samuel Echols <samuel.echols@selu.edu>
2022-08-28 16:23:00 -0500

    fix: fixed indexing and override the _id property

--- src/database/models/EconomyModel.ts
+++ src/database/models/EconomyModel.ts
@@ -13,14 +13,39 @@ interface IEconomy {
 }
 
 const EconomySchema = new Schema({
-  id: { type: String, ref: "User", index: true },
-  lastUse: { type: Schema.Types.Date, default: Date.now() },
-  coins: { type: Number, index: -1, default: 0 },
-  bank: { type: Number, index: -1, default: 0 },
-  xp: { type: Number, index: -1, default: 0 },
-  level: { type: Number, default: 0 },
-  items: { type: Array, default: [] },
-  bracket: { type: Number, default: 0 },
+  _id: { 
+    type: String, 
+    ref: "User", 
+    index: true 
+  },
+  lastUse: { 
+    type: Schema.Types.Date, 
+    default: Date.now() 
+  },
+  coins: { 
+    type: Number,
+    default: 0 
+  },
+  bank: { 
+    type: Number,  
+    default: 0 
+  },
+  xp: { 
+    type: Number, 
+    default: 0 
+  },
+  level: { 
+    type: Number, 
+    default: 0 
+  },
+  items: { 
+    type: Array, 
+    default: [] 
+  },
+  bracket: { 
+    type: Number, 
+    default: 0 
+  },
 });
 
 export const Economy = model<IEconomy>("stats", EconomySchema);
