11f2a96e2a995653dbf51ec12d4bc11d3eca92c1
Samuel Echols <samuel.echols@selu.edu>
2022-08-28 18:45:02 -0500

    feat: deprecated findOneOrCreate

--- src/database/functions/findOneOrCreate.ts
+++ src/database/functions/findOneOrCreate.ts
@@ -1,6 +1,9 @@
-import { EconomyModel } from "../models/EconomyModel";
+/**
+ * @deprecated Use the findById() function instead
+ */
+import { Economy } from "../models/EconomyModel";
 
-export default async function (query: object, createObject: object, Model: typeof EconomyModel, lean = true) {
+export default async function (query: object, createObject: object, Model: typeof Economy, lean = true) {
   let find;
   if (lean) find = await Model.findOne(query).lean();
   else find = await Model.findOne(query);
