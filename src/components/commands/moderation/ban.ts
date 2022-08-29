195abc0f470ad0b2db63cef4bde8a2000ff54963
Samuel Echols <samuel.echols@selu.edu>
2022-08-28 20:58:29 -0500

    fix: fixed ban

--- src/components/commands/moderation/ban.ts
+++ src/components/commands/moderation/ban.ts
@@ -4,28 +4,33 @@ import { Command, ChatInputCommand, Args } from "@sapphire/framework";
 
 @ApplyOptions<Command.Options>
 (
-  {
-    name: "ban",
-    description: "Bans a user",
-    requiredUserPermissions: ['BAN_MEMBERS']
-  }
+	{
+		name: "ban",
+		description: "Bans a user",
+		requiredUserPermissions: ['BAN_MEMBERS']
+	}
 )
 export class BanCommand extends Command {
 
-  public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
-    registry.registerChatInputCommand((builder) =>
-      builder
-        .setName(this.name)
-        .setDescription(this.description)
-        .setDMPermission(false)
-    )
-  }
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.setDMPermission(false)
+		)
+	}
+	public async messageRun(message: Message, args: Args) {
 
-  public async messageRun(message: Message, args: Args) {
+		const userToBan = await args.pick('member');
+		const reason = await args.pick('string').catch(() => "None");
 
-    const userToBan = await args.pick('member');
-    const reason = await args.pick('string').catch(() => "None");
-    userToBan.ban({ reason: reason });
-    message.reply(`Successfully banned ${userToBan} with Reason: ${reason}`)
-  }
+		if (!userToBan.bannable || !userToBan.kickable) {
+        return message.reply("You cannot ban this user!")
+
+		} else {
+			userToBan.ban({ reason: reason });
+			return message.reply(`Successfully banned ${userToBan} with Reason: ${reason}`)
+		}
+	}
 }
