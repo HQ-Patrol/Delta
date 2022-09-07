ecbb601276253bc3fea1d5900b160619a95c30b3
Leo <leowasntfound@gmail.com>
2022-09-07 19:01:09 +1000

    feat: add prompt jsdoc

--- src/utilities/prompt/YesNoPrompt.ts
+++ src/utilities/prompt/YesNoPrompt.ts
@@ -9,6 +9,16 @@ import {
 import { MessageComponentTypes } from "discord.js/typings/enums";
 import emoji from "../../constants/emoji";
 
+/**
+ * Creates a yes or no prompt. 
+ * Returns null if prompt was unreplied to, and a boolean representing the response.
+ *
+ * @param interaction {CommandInteraction}
+ * @param askMessage {string}
+ * @param yesButtonLabel {string}
+ * @param noButtonLabel {string} 
+ * @returns {Promise<[boolean | null, Message]>} an array consisting of a result and the prompt message
+ */
 export async function YesNoPrompt(
   interaction: CommandInteraction,
   askMessage: string,
@@ -46,11 +56,10 @@ export async function YesNoPrompt(
       return true;
     }
 
-    if (int.isButton())
-      int.reply({
-        content: "This menu is not for you.",
-        ephemeral: true,
-      });
+    int.reply({
+      content: "This menu is not for you.",
+      ephemeral: true,
+    });
 
     return false;
   };
