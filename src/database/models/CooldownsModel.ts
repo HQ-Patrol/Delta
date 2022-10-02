58c30979215c37123d6fa0bbf8b6cc77704eb708
Leo <leowasntfound@gmail.com>
2022-10-02 12:52:41 +1000

    fix: cooldownsmodel

--- src/database/models/CooldownsModel.ts
+++ src/database/models/CooldownsModel.ts
@@ -1,27 +1,3 @@
-import { Schema, model } from "mongoose";
-
-export interface ICooldowns {
-  id: String,
-	days: Number,
-	next: Number,
-	resets: Number,
-	weeklyReset: String,
-	boosterReset: String,
-	voteReset: Number,
-	taxReturnReset: String,
-	taxBenefitReset: String
-}
-
-const CooldownSchema = new Schema<ICooldowns>({
-  id: String,
-  days: { 
-    type: Number, 
-    default: 0
-  }
-});
-
-export const CooldownsModel = model<ICooldowns>("cooldowns", CooldownSchema);
-
 import { Schema, model } from "mongoose"; 
  
 export interface ICooldowns { 
