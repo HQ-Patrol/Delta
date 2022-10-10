3fa27fabe1e055b19c79806144213f9ef4423616
Leo <leowasntfound@gmail.com>
2022-10-10 17:49:49 +1100

    feat: help recode

--- src/components/commands/information/help.ts
+++ src/components/commands/information/help.ts
@@ -1,37 +1,161 @@
-/*
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApplyOptions } from "@sapphire/decorators";
 import { ApplicationCommandRegistry, ChatInputCommand, Command } from "@sapphire/framework";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import Hjson from 'hjson';
-import AdvancedComponentMenu from '../../../utilities/classes/ComponentMenu';
-import FS from 'node:fs';
-import { MessageActionRow, MessageComponentInteraction, MessageSelectMenu } from "discord.js";
-const fs = FS.promises;
+import { ButtonInteraction, MessageActionRow, MessageActionRowComponent, MessageButton, MessageEmbed, MessageSelectMenu, SelectMenuInteraction } from "discord.js";
+import { readFile } from "node:fs/promises";
+import { parse } from "hjson";
+import emoji from "../../../constants/emoji";
+import ComponentMenu from "../../../utilities/classes/ComponentMenu";
 
-let guide :any;
+let guide: Record<string, any> | null = null;
+export async function getGuide() {
+    if(!guide) {
+        let data = await readFile("./src/data/json/guide.hjson", "utf-8");
+        // replace variables, etc
+        data = data.
+            replace(/\\/g, "`")
+            .replace(/{prefix}/g, "/") // due to recode
+            .replace(/%(.+?)%/g, (_: unknown, cap: string) => (emoji as any)[cap]);
+        guide = await parse(data);
+    } 
+    return guide;
+}
+
+// cache
+const buttons: Record<string, MessageActionRowComponent[]> = {};
+let selectMenu: MessageActionRow | null = null;
 
 @ApplyOptions<Command.Options>({
     name: "help",
-    description: "Explains the usage of the bot"
+    description: "Want to know more about Patrol Bot? Run this command!"
 })
-
-export class HelpCommand extends Command{
+export class HelpCommand extends Command {
     public registerApplicationCommands(registry: ApplicationCommandRegistry) {
         registry.registerChatInputCommand((builder) =>
-        builder
-            .setName(this.name)
-            .setDescription(this.description)
+            builder
+                .setName(this.name)
+                .setDescription(this.description)
         )
     }
-    public async chatInputRun(interaction: ChatInputCommand.Interaction){
+    public async chatInputRun(interaction: ChatInputCommand.Interaction) {
+        const guide = await getGuide();
+        if(!guide) return;
 
-        // Help command
-        if(!guide) {
-            const data = await fs.readFile("../../../data/json/guide.hjson", "utf-8");
-            guide = await Hjson.parse(data);
+        let selectedOption = "mainMenu"; // the "category"
+        let selectedMenu: string | null = null; // the tabs of the category, null defaults to the default menu
+
+        const handler = (int: SelectMenuInteraction | ButtonInteraction) => {
+            int.deferUpdate(); 
+            if(int.isSelectMenu()) {
+                // change selected option
+                selectedOption = int.values?.[0];
+                selectedMenu = null; // default menu
+            } else {
+                // change menu
+                selectedMenu = int.customId;
+            }
+            update();
         }
 
-        // Main Command
+        const update = (init = false) => {
+            const option = guide[selectedOption];
+            if(selectedMenu == null) {
+                const values = Object.entries(option.menus);
+                const find = values.find((menu: any) => menu[1]?.default === true);
+                selectedMenu = find?.[0] || values[0][0];
+            }
+
+            // select menu:
+            if(!selectMenu) {
+                console.log("GENERATING SELECT MENU");
+                // generate select menu, otherwise just fetch from cache
+                const options = [];
+                for(const [optionId, option] of Object.entries(guide)) {
+                    options.push({
+                        label: option.selectMenuLabel,
+                        value: optionId,
+                        description: option.description,
+                        emoji: option.emoji,
+                        default: false
+                    })
+                }
+
+                selectMenu = new MessageActionRow()
+                    .addComponents(
+                        new MessageSelectMenu()
+                            .setCustomId("select")
+                            .setPlaceholder("Learn more about Patrol Bot!")
+                            .addOptions(options)
+                    )
+                   
+            }
+
+            // embed:
+            const embedInfo = guide[selectedOption].menus[selectedMenu].embed;
+            const embed = new MessageEmbed()
+            embedInfo?.title && embed.setTitle(embedInfo.title);
+            embedInfo?.description && embed.setDescription(embedInfo
+                .description
+                .replace(/\\/g, "`")
+                .replace(/{prefix}/g, "/") // due to recode
+                .replace(/%(.+?)%/g, (_: unknown, cap: string) => (emoji as any)[cap]));
+            embedInfo?.color && embed.setColor(embedInfo.color);
+
+            // buttons
+            if(!buttons[selectedOption]) {
+                console.log("GENERATING BUTTONS FOR " + selectedOption);
+                // generate buttons for the option
+                const row = [];
+                for(const m in option.menus) {
+                    const component = option.menus[m].component;
+                    if(component?.link) {
+                        row.push(
+                            new MessageButton()
+                                .setURL(component.url)
+                                .setLabel(component.label)
+                                .setStyle("LINK")
+                        )
+                    } else {
+                        row.push(
+                            new MessageButton()
+                                .setCustomId(m)
+                                .setLabel(component.label)
+                                .setStyle(component.style)
+                        )
+                    }
+                }
+                buttons[selectedOption] = row;
+            }
+
+            // override
+            const components = buttons[selectedOption];
+            const find = components.find((c) => c.customId === selectedMenu);
+            if(find) find.disabled = true;
+            const buttonRow = new MessageActionRow().setComponents(components);
+            const menuRow = selectMenu;
+
+            menu.embed = embed;
+            if(init) {
+                menu.components = [buttonRow, menuRow];
+                menu.send();
+            } else {
+                menu.components[0] = buttonRow;
+                menu.update();
+            }
+            if(find) find.disabled = false;
+        }
+
+        const menu = new ComponentMenu(interaction)
+            .whitelist(interaction.user.id)
+            .listen(handler);
+
+        update(true);
+    }
+}
+
+
+/*
+ // Main Command
         let selectedOption = "mainMenu";
         let selectedMenu = false;
 
@@ -135,6 +259,4 @@ export class HelpCommand extends Command{
 
         menu.components = await updateComponents(true);
         await menu.send();
-    }
-}
-*/
+        */
