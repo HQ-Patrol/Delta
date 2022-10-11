2b70b3e6451749161a97a40d32549cbee1bffb45
Leo <leowasntfound@gmail.com>
2022-10-11 18:28:13 +1100

    fix: remove redundant logging

--- src/components/commands/information/help.ts
+++ src/components/commands/information/help.ts
@@ -67,7 +67,6 @@ export class HelpCommand extends Command {
 
             // select menu:
             if(!selectMenu) {
-                console.log("GENERATING SELECT MENU");
                 // generate select menu, otherwise just fetch from cache
                 const options = [];
                 for(const [optionId, option] of Object.entries(guide)) {
@@ -103,7 +102,6 @@ export class HelpCommand extends Command {
 
             // buttons
             if(!buttons[selectedOption]) {
-                console.log("GENERATING BUTTONS FOR " + selectedOption);
                 // generate buttons for the option
                 const row = [];
                 for(const m in option.menus) {
@@ -152,111 +150,3 @@ export class HelpCommand extends Command {
         update(true);
     }
 }
-
-
-/*
- // Main Command
-        let selectedOption = "mainMenu";
-        let selectedMenu = false;
-
-
-        const handler = async (int: MessageComponentInteraction) => {
-            await int.deferUpdate();
-            if(int.isSelectMenu()) {
-                // Change selected option, reset selected menu to default.
-                selectedOption = int.values?.[0];
-                selectedMenu = null;
-                await updateComponents();
-            } else if(int.isButton()) {
-                selectedMenu = int.customId;
-                await updateComponents();
-            }
-        };
-
-
-        const selectMenu = () => {
-            const selectMenuOptions = [];
-            for(const [key, value] of Object.entries(guide)) {
-                selectMenuOptions.push({
-                    label: (value as any).selectMenuLabel,
-                    value: key,
-                    description: (value as any).description,
-                    emoji: (value as any).emoji,
-                    default: (selectedOption === key)
-                });
-            }
-            return new MessageActionRow()
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId("select")
-                        .setPlaceholder("Learn more about Patrol Bot!")
-                        .addOptions(selectMenuOptions)
-                );
-        };
-
-        const updateComponents = async (returnComponents = false) => {
-            const menus = Object.entries(guide[selectedOption].menus);
-
-            // Update Embed
-            let foundEmbed;
-            if(!selectedMenu) {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore TODO : FIX THIS LATER
-                foundEmbed = (menus?.find(m => m[0] === selectedMenu)![1] as any).embed;
-            } else {
-                const found = menus.find(m => m[1].default === true);
-                foundEmbed = found[1].embed;
-                selectedMenu = found[0];
-            }
-
-            const embed = new Discord.MessageEmbed();
-            foundEmbed?.title && embed.setTitle(foundEmbed.title);
-            foundEmbed?.description && embed.setDescription(foundEmbed.description.replace(/\\/g, "`").replace(/{prefix}/g, prefix));
-            foundEmbed?.color && embed.setColor(foundEmbed.color);
-            menu.embed = embed;
-
-            // Update buttons
-            const buttonRow = new Discord.MessageActionRow();
-            // Buttons
-            for(const [key, value] of menus) {
-                if(value.component.link) {
-                    buttonRow.addComponents(
-                        new Discord.MessageButton()
-                            .setLabel(value.component.label)
-                            .setStyle("LINK")
-                            .setURL(value.component.url)
-                    );
-                } else {
-                    buttonRow.addComponents(
-                        new Discord.MessageButton()
-                            .setLabel(value.component.label)
-                            .setStyle(value.component.style)
-                            .setCustomId(key)
-                            .setDisabled(selectedMenu === key)
-                    );
-                }
-            }
-
-            if(returnComponents) {
-                return [buttonRow, selectMenu()];
-            } else {
-                menu.components = [buttonRow, selectMenu()];
-
-                // update!
-                return await menu.update();
-            }
-        };
-
-        const mainMenuEmbed = guide["mainMenu"].menus["mainMenu"].embed;
-        const menu = new AdvancedComponentMenu(message)
-            .setDefaultEmbed(
-                new Discord.MessageEmbed()
-                    .setTitle(mainMenuEmbed.title)
-                    .setDescription(mainMenuEmbed.description.replace(/\\`/g, "`").replace(/{prefix}/g, prefix))
-                    .setColor(mainMenuEmbed.color)
-            )
-            .setHandler(handler);
-
-        menu.components = await updateComponents(true);
-        await menu.send();
-        */
