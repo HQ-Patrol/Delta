7a06b43b0ccb21070ce49b198b4e917e6cbfda1a
PseudoSuperior <69754594+PseudoSuperior@users.noreply.github.com>
2022-12-11 00:03:40 +0530

    edits

--- src/components/listeners/interactionCreate.ts
+++ src/components/listeners/interactionCreate.ts
@@ -9,7 +9,7 @@ export class InteractionCreateListener extends Listener {
   public async run(interaction: Interaction) {
     if (interaction.isCommand()) {
       return interaction.reply({
-        content: "Sin Edit THis",
+        content: `*Unfortunately, __Patrol bot will be OFFLINE till January 2023__* <a:exclamation:741988026296696872>\n<a:Right:763251259896758282> If you still want to Use Patrol Bot, Visit our Official Server: https://discord.gg/HQ or [__**CLICK HERE TO ADD A TEMPORARY VERSION**__](https://discord.com/api/oauth2/authorize?client_id=1014553059956883507&permissions=8&scope=bot)`,
         ephemeral: true,
       });
     }
